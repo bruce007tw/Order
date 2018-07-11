af71ab6b5c052fdcc5936af95a40d99f0379cbaa
bruce007tw <34640846+bruce007tw@users.noreply.github.com>
2018-07-11 11:51:51 +0800

    2018_07_11_A

---
+++ app/src/main/java/com/bruce007tw/order/MenuActivity.java
@@ -0,0 +1,13 @@
+package com.bruce007tw.order;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
